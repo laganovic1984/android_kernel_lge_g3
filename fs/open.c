ec0689a3b423f41f7c1a764204661af854617936
manups4e <manups4e@gmail.com>
2015-07-12 00:08:59 +0200

    vfs: check if f_count is 0 or negative filp_close is using !file_count(filp) to check if f_count is 0. if it is 0, filp_close think it is a closed file then will return. However, for a closed file, f_count could be reduced to -1, then !file_count(filp) is false, filp_close will proceed to handle this file then could panic. This change will check if f_count is 0 or negative instead of only checking 0 to avoid panic.
    b/18200219 LRX21M: kernel_panic
    Change-Id: I3881574d4971bf4e0bc261cf3459451600ce6a27
    
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- fs/open.c
+++ fs/open.c
@@ -1061,10 +1061,12 @@ SYSCALL_DEFINE2(creat, const char __user *, pathname, umode_t, mode)
 int filp_close(struct file *filp, fl_owner_t id)
 {
 	int retval = 0;
+	long ret;
 
-	if (!file_count(filp)) {
-		printk(KERN_ERR "VFS: Close: file count is 0\n");
-		return 0;
+	ret = file_count(filp);
+	if (ret <= 0) {
+		printk(KERN_ERR "VFS: Close: file count is %ld\n", ret);
+		WARN_ON(ret < 0);		return 0;
 	}
 
 	if (filp->f_op && filp->f_op->flush)
