78abbb04c3f1d7e2aa52f2926c668a60939f76cb
manups4e <manups4e@gmail.com>
2015-10-10 09:20:46 +0200

    ASoC: msm: fix compress codec capabilies part 2

--- lge/g3/sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ lge/g3/sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -126,7 +126,7 @@ struct msm_compr_gapless_state {
 };
 
 static unsigned int supported_sample_rates[] = {
-       8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
+	8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
 };
 
 struct msm_compr_pdata {
@@ -1606,8 +1606,11 @@ static int msm_compr_get_codec_caps(struct snd_compr_stream *cstream,
 		codec->num_descriptors = 4;
 #endif
 		codec->descriptor[0].max_ch = 2;
-		/* FIXME sample_rates in Hz */
-		codec->descriptor[0].sample_rates = SNDRV_PCM_RATE_8000_48000;
+		memcpy(codec->descriptor[0].sample_rates,
+		       supported_sample_rates,
+		       sizeof(supported_sample_rates));
+		codec->descriptor[0].num_sample_rates =
+			sizeof(supported_sample_rates)/sizeof(unsigned int);
 		codec->descriptor[0].bit_rate[0] = 320; /* 320kbps */
 		codec->descriptor[0].bit_rate[1] = 128;
 		codec->descriptor[0].num_bitrates = 2;
@@ -1622,8 +1625,11 @@ static int msm_compr_get_codec_caps(struct snd_compr_stream *cstream,
 		codec->num_descriptors = 4;
 #endif
 		codec->descriptor[1].max_ch = 2;
-		/* FIXME sample_rates in Hz */
-		codec->descriptor[1].sample_rates = SNDRV_PCM_RATE_8000_48000;
+		memcpy(codec->descriptor[1].sample_rates,
+		       supported_sample_rates,
+		       sizeof(supported_sample_rates));
+		codec->descriptor[1].num_sample_rates =
+			sizeof(supported_sample_rates)/sizeof(unsigned int);
 		codec->descriptor[1].bit_rate[0] = 320; /* 320kbps */
 		codec->descriptor[1].bit_rate[1] = 128;
 		codec->descriptor[1].num_bitrates = 2;
@@ -1641,7 +1647,11 @@ static int msm_compr_get_codec_caps(struct snd_compr_stream *cstream,
 	case SND_AUDIOCODEC_PCM:
 		codec->num_descriptors = 5;
 		codec->descriptor[4].max_ch = 2;
-		codec->descriptor[4].sample_rates = SNDRV_PCM_RATE_8000_192000;
+		memcpy(codec->descriptor[4].sample_rates,
+		       supported_sample_rates,
+		       sizeof(supported_sample_rates));
+		codec->descriptor[4].num_sample_rates =
+			sizeof(supported_sample_rates)/sizeof(unsigned int);
 		codec->descriptor[4].bit_rate[0] = 192; /* 192kbps */
 		codec->descriptor[4].bit_rate[1] = 8;
 		codec->descriptor[4].num_bitrates = 2;
